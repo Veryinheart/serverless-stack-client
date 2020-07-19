cbe186e81b2c4f5a402ad29df678c94a572fe638
danielJing <danieljing1993@gmail.com>
2020-07-19 19:33:48 +1000

    setting up react app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+      REGION: "us-east-2",
+      BUCKET: "serverlessstackdemo"
+    },
+    apiGateway: {
+      REGION: "us-east-2",
+      URL: "https://ikrt4ajs99.execute-api.us-east-2.amazonaws.com/prod"
+    },
+    cognito: {
+      REGION: "us-east-2",
+      USER_POOL_ID: "us-east-2_pLpmXtJjb",
+      APP_CLIENT_ID: "5plo6mdu0fa3jri2ae0o03atl",
+      IDENTITY_POOL_ID: "us-east-2:b566fe45-be53-4e66-bb8b-04e29016a14e"
+    }
+  };
